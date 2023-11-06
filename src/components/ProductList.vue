229789a2034bfbc8f846e86ddf7045fe36db2d72
gabriel <gabi8666091@gmail.com>
2023-11-06 10:45:50 -0400

    bugs

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,28 +1,8 @@
 <template>
     <div class="card">
 
-        <div v-if="loading" class="flex flex-row flex-wrap">
-
-            <div v-for="i in 10" :key="i" class="col-12 sm:col-6 lg:col-4 xl:col-3 p-2">
-                <div  class="p-4 border-1 surface-border surface-card border-round">
-                    <div class="flex flex-wrap align-items-center justify-content-between gap-2">
-                        <Skeleton class="w-6rem border-round h-2rem" />
-                        <Skeleton class="w-3rem border-round h-1rem" />
-                    </div>
-                    <div class="flex flex-column align-items-center gap-3 py-5">
-                        <Skeleton class="w-9 shadow-2 border-round h-10rem" />
-                        <Skeleton class="w-8rem border-round h-2rem" />
-                        <Skeleton class="w-6rem border-round h-1rem" />
-                    </div>
-                    <div class="flex align-items-center justify-content-between">
-                        <Skeleton class="w-4rem border-round h-2rem" />
-                        <Skeleton shape="circle" class="w-3rem h-3rem" />
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    <DataView v-else :value="props.products" :layout="layout">
+      
+    <DataView :value="props.products" :layout="layout">
 
 
 
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -10,8 +10,28 @@
         </div>
 
     </div>
-    <div v-else>
-        <h2>Cargando...</h2>
+    <div v-else class="flex flex-row flex-wrap">
+     
+
+<div v-for="i in 10" :key="i" class="col-12 sm:col-6 lg:col-4 xl:col-3 p-2">
+    <div  class="p-4 border-1 surface-border surface-card border-round">
+        <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+            <Skeleton class="w-6rem border-round h-2rem" />
+            <Skeleton class="w-3rem border-round h-1rem" />
+        </div>
+        <div class="flex flex-column align-items-center gap-3 py-5">
+            <Skeleton class="w-9 shadow-2 border-round h-10rem" />
+            <Skeleton class="w-8rem border-round h-2rem" />
+            <Skeleton class="w-6rem border-round h-1rem" />
+        </div>
+        <div class="flex align-items-center justify-content-between">
+            <Skeleton class="w-4rem border-round h-2rem" />
+            <Skeleton shape="circle" class="w-3rem h-3rem" />
+        </div>
+    </div>
+</div>
+
+
     </div>
 </template>
 
